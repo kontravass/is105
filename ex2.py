5199ec7ecba4b8ec388714cfd93752ccf74eb836
kontravass <jornib@online.no>
2014-01-29 14:22:07 +0100

    new file:   ex1.py 	new file:   ex2.py 	new file:   ex3.py 	new file:   ex4.py 	new file:   ex5.py 	new file:   ex6.py 	new file:   ex7.py 	new file:   ex8.py 	new file:   ex9.py

---
+++ ex2.py
@@ -0,0 +1,8 @@
+# Det er saann vi kommenterer... hash-tag er tingen
+
+print "I could have code like this." #en kommentar etter blir ignorert.
+
+#du kan ogsp kommentere vekk en programkode, som du ikke vil kjoere.
+#print "Denne vil ikke kjoere."
+
+print "Denne vil kjoere."
---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Zed A. Shaw'
+my_age = 35
+my_height = 74
+my_weight = 180
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's", my_weight, "pounds heavy." #For aa vise at det gaar an aa gjoere det saann ogsaa.
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+#Vanskelig linje. Proev aa faa den riktig.
+print "If I add %d, %d and %d I get %d." %(my_age, my_height, my_weight, my_age + my_height + my_weight)
